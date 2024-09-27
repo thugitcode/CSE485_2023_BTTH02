cb75ca759309e60c8a12e04fc222bfbd72d90d00
thugitcode <153116032+thugitcode@users.noreply.github.com>
2024-09-28 04:45:07 +0700

    Update login.php

--- views/admin/login.php
+++ views/admin/login.php
@@ -24,10 +24,10 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Trang chủ</a>
+                    <a class="nav-link" aria-current="page" href="http://localhost:3000/index.php?controller=home&action=index">Trang chủ</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link active" href="./login.php">Đăng nhập</a>
+                    <a class="nav-link active" href="http://localhost:3000/index.php?controller=Account&action=login">Đăng nhập</a>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
