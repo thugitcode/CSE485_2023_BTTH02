0f55d0d3a9e441c3d92c1d45f5f4e1efa36cd2d1
thugitcode <153116032+thugitcode@users.noreply.github.com>
2024-09-28 04:07:39 +0700

    Update AuthorModel.php

--- models/AuthorModel.php
+++ models/AuthorModel.php
@@ -1,30 +1,28 @@
 <?php
-class Author {
-    public $ma_tgia; // Mã tác giả
-    public $ten_tgia; // Tên tác giả
+class Author{
+    public $ma_tgia;
+    public $ten_tgia;
 
     public function __construct($ma_tgia, $ten_tgia) {
         $this->ma_tgia = $ma_tgia;
         $this->ten_tgia = $ten_tgia;
     }
 
-    // Getter cho mã tác giả
-    public function get_matgia() {
+    public function get_matgia(){
         return $this->ma_tgia;
     }
 
-    // Getter cho tên tác giả
-    public function get_ten_tgia() {
+    public function get_ten_tgia(){
         return $this->ten_tgia;
     }
 
-    // Setter cho mã tác giả
-    public function set_matgia($ma_tgia) {
+    public function set_matgia( $ma_tgia ) {
         $this->ma_tgia = $ma_tgia;
     }
 
-    // Setter cho tên tác giả
-    public function set_ten_tgia($ten_tgia) {
+    public function set_ten_tgia( $ten_tgia ) {
         $this->ten_tgia = $ten_tgia;
     }
 }
+
+?>
