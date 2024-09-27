72ef17e31bd8897407185a3feaf64c5d6c8c6cc1
thugitcode <153116032+thugitcode@users.noreply.github.com>
2024-09-28 04:07:01 +0700

    Update AuthorController.php

--- controllers/AuthorController.php
+++ controllers/AuthorController.php
@@ -1,76 +1,76 @@
 <?php
-include('services/AuthorService.php'); 
+include('services/AuthorService.php');
 
 class AuthorController {
     
     public function index() {
-        $authorService = new AuthorService(); 
+        $authorService = new AuthorService();
         $authors = $authorService->getAllAuthors();        
-        // Render ra index của tác giả
-        include 'views/author/index.php'; // Thay đổi đường dẫn đến file views tương ứng
+        //render ra index của Author
+        include 'views/author/index.php';
     }
 
-    public function add() {
+    public function add(){
         $error = '';
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $authorName = $_POST['ten_tgia']; 
-            $authorService = new AuthorService(); 
+            $authorService = new AuthorService();
 
-            if ($authorService->isAuthorExist($authorName)) { 
-                $error = 'Tác giả đã tồn tại.'; // Thay đổi thông báo lỗi cho phù hợp
+            if ($authorService->isAuthorExist($authorName)) {
+                $error = 'Thể loại đã tồn tại.';
             } else {
-                $author = $authorService->addAuthor($authorName); 
+                $author = $authorService->addAuthor($authorName);
 
-                header('Location: http://localhost:3000/index.php?controller=Author&action=index'); // Chuyển hướng về danh sách tác giả
+                header('Location: http://localhost:3000/index.php?controller=Author&action=index'); // Chuyển hướng về danh sách thể loại
                 exit();
             }
         }
 
-        include 'views/author/add_author.php'; // Thay đổi đường dẫn đến file form thêm mới tác giả
+        include 'views/author/add_author.php'; // Hiển thị form thêm mới với thông báo lỗi
     }
 
-    // Gửi thông tin tác giả theo mã
+    //gửi tt thể loại theo mã
+    // Kiểm tra nếu mã thể loại đã được gửi qua URL
     public function edit() {
-        if (isset($_GET['ma_tgia'])) { 
+        if (isset($_GET['ma_tgia'])) {
             $ma_tgia = $_GET['ma_tgia'];
 
-            $authorService = new AuthorService(); 
-            // Lấy thông tin tác giả theo mã
-            $author = $authorService->getAuthorById($ma_tgia); 
+            $authorService = new AuthorService();
+            // Lấy thông tin thể loại theo mã
+            $author = $authorService->getAuthorById($ma_tgia);
             
             if ($author) {
-                include 'views/author/edit_author.php'; // Thay đổi đường dẫn đến file form chỉnh sửa tác giả
+            include 'views/author/edit_author.php'; // Bao gồm file form chỉnh sửa
             } else {
-                echo "Không tìm thấy tác giả."; // Thay đổi thông báo lỗi cho phù hợp
+                echo "Không tìm thấy thể loại.";
             }
-        } else {
-            echo "Mã tác giả không hợp lệ."; // Thay đổi thông báo lỗi cho phù hợp
+            } else {
+                echo "Mã thể loại không hợp lệ.";
+            }
         }
-    }
-
-    public function update() {
+    public function update(){
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $authorService = new AuthorService(); 
-            $authorService->updateAuthor($_POST['ma_tgia'], $_POST['ten_tgia']); 
+            $authorService = new AuthorService();
+            $authorService->updateAuthor($_POST['ma_tgia'], $_POST['ten_tgia']);
 
             header('Location: http://localhost:3000/index.php?controller=Author&action=index'); // Chuyển hướng về trang index sau khi cập nhật thành công
             exit();
         } else {
-            include("views/author/update_author.php"); // Thay đổi đường dẫn đến file cập nhật tác giả
+            include("views/author/update_author.php");
         }
     }
 
-    public function delete() {
+    public function delete(){
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $authorService = new AuthorService(); 
-            $authorService->deleteAuthor($_POST['ma_tgia']); 
+            $authorService = new AuthorService();
+            $authorService->deleteAuthor($_POST['ma_tgia']);
 
             header('Location: http://localhost:3000/index.php?controller=Author&action=index'); // Chuyển hướng về trang index sau khi xóa thành công
             exit();
         } else {
-            include("views/author/index.php"); // Thay đổi đường dẫn đến file index của tác giả
+            include("views/author/index.php");
         }
     }
-}
+    }
 ?>
