f8dc7b3c9bcc4e98056fa6f6e54e4a03de86cee7
thugitcode <153116032+thugitcode@users.noreply.github.com>
2024-09-28 04:52:35 +0700

    Update index.php

--- views/category/index.php
+++ views/category/index.php
@@ -21,21 +21,21 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="../home.php">Trang chủ</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../index.php">Trang ngoài</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active fw-bold" href="category.php">Thể loại</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../author/author.php">Tác giả</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../article/article.php">Bài viết</a>
-                    </li>
+                </li>
+                <a class="nav-link" aria-current="page" href="http://localhost:3000/index.php?controller=Admin&action=index">Trang chủ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://localhost:3000/index.php?controller=home&action=index">Trang ngoài</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active fw-bold" href="http://localhost:3000/index.php?controller=Category&action=index">Thể loại</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://localhost:3000/index.php?controller=Author&action=index">Tác giả</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://localhost:3000/index.php?controller=Article&action=index">Bài viết</a>
+                        </li>
                 </ul>
                 </div>
             </div>
